b87ea69660c907e96d23033af8eadbe57f6704dd
sizaguir <138946181+sizaguir@users.noreply.github.com>
2025-11-12 18:52:09 +0000

    diseño del dashboard

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Grid } from '@mui/material';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+
+      {/* Encabezado */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado        
+      </Grid>
+
+      {/* Alertas */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+
+      {/* Selector */}
+      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+
+      {/* Indicadores */}
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+
+      {/* Gráfico */}
+      <Grid sx={{ display: { xs: "none", md: "block" } }} >
+        Elemento: Gráfico
+      </Grid>
+
+      {/* Tabla */}
+      <Grid sx={{ display: { xs: "none", md: "block" } }} >Elemento: Tabla</Grid>
+
+      {/* Información adicional */}
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+    </Grid>
+  )
+}
+
+export default App

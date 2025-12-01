92c71338af4ce9f2ba2bd06e3f191b803a9dfc8f
sizaguir <138946181+sizaguir@users.noreply.github.com>
2025-12-01 19:12:53 +0000

    creacion componente Selector.UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 import './App.css'
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 import { Grid } from '@mui/material';
 
@@ -24,7 +25,9 @@ function App() {
       </Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3 }}>
+        <SelectorUI/>
+      </Grid>
 
       {/* Indicadores */}
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>

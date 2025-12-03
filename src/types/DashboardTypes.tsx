055a9acd03b415ee6955bb5f7fbff5c5c5a2043e
sizaguir <138946181+sizaguir@users.noreply.github.com>
2025-12-03 18:41:54 +0000

    interfaces añadidas

--- src/types/DashboardTypes.tsx
+++ src/types/DashboardTypes.tsx
@@ -1,36 +1,41 @@
 export interface OpenMeteoResponse {
-    latitude: number;
-    longitude: number;
-    generationtime_ms: number;
-    utc_offset_seconds: number;
-    timezone: string;
-    timezone_abbreviation: string;
-    elevation: number;
-    current: Current;
-    hourly: Hourly;
-    hourly_units: HourlyUnits;
+  latitude: number
+  longitude: number
+  generationtime_ms: number
+  utc_offset_seconds: number
+  timezone: string
+  timezone_abbreviation: string
+  elevation: number
+  current_units: CurrentUnits
+  current: Current
+  hourly_units: HourlyUnits
+  hourly: Hourly
+}
+
+export interface CurrentUnits {
+  time: string
+  interval: string
+  temperature_2m: string
+  relative_humidity_2m: string
+  apparent_temperature: string
+  wind_speed_10m: string
 }
 
 export interface Current {
-    temperature_2m: number;
-    relative_humidity_2m: number;
-    apparent_temperature: number;
-    wind_speed_10m: number;
-    time: string;
-}
-
-export interface Hourly {
-    time: string[];
-    temperature_2m: number[];
-    relative_humidity_2m: number[];
-    apparent_temperature: number[];
-    wind_speed_10m: number[];
+  time: string
+  interval: number
+  temperature_2m: number
+  relative_humidity_2m: number
+  apparent_temperature: number
+  wind_speed_10m: number
 }
 
 export interface HourlyUnits {
-    time: string;
-    temperature_2m: string;
-    relative_humidity_2m: string;
-    apparent_temperature: string;
-    wind_speed_10m: string;
+  time: string
+  temperature_2m: string
+}
+
+export interface Hourly {
+  time: string[]
+  temperature_2m: number[]
 }
